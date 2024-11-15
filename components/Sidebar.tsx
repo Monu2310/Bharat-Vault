0391671084c30c50aa277e86ed29edd571fca503
Monu2310 <88082379+Monu2310@users.noreply.github.com>
2024-11-16 04:39:51 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = ({ fullName, avatar, email }: Props) => {
     <aside className="sidebar">
       <Link href="/">
         <Image
-          src="/public/assets/icons/logo-full-brand.png"
+          src="/public/assets/icons/logo-full-brand.svg"
           alt="logo"
           width={160}
           height={50}
